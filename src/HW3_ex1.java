cb90cccad8123c49c93512f3d592219acc1c41d8
elsaneth <elisabeth.suits@gmail.com>
2024-03-26 22:52:42 +0200

    added documentation

--- src/HW3_ex1.java
+++ src/HW3_ex1.java
@@ -1,11 +1,15 @@
 import java.util.*;
 
 public class HW3_ex1 {
+    /**
+     * Main method to demonstrate string manipulation operations.
+     * Calls methods to find the most frequent character in a string and modify the string accordingly.
+     * If the result for the method findMostFrequentNr is null, then other methods are not executed.
+     *
+     * @param args Command-line arguments (not used in this implementation).
+     * @return void
+     */
     public static void main(String[] args) {
-        /*
-        Calling out methods. If result for method findMostFrequentNr is null, then it does not
-        run other method.
-         */
         String ln = "trrkrt kpsstVVrqqKKt !!!!!!!!!!!!";
         System.out.println("Intial String: " + ln);
         char mostFrequentNr = findMostFrequentNr(ln);
@@ -16,10 +20,17 @@ public class HW3_ex1 {
         }
     }
 
+    /**
+     * Finds the most frequent digit character in the given string and returns it.
+     * If the string contains no digit characters, it returns the null character '\0'.
+     *
+     * @param line The input string to search for the most frequent digit character.
+     * @return The most frequent digit character in the string, or '\0' if no digits are found.
+     */
     public static char findMostFrequentNr(String line) {
         /*
-        Make new list for numbers. Goes through the String and if an element in String is digit,
-        then it adds it to the list.
+        Creates a new list to store digit characters. Iterates through the string, and if a character
+        in the string is a digit, adds it to the list.
          */
         List<Character> numbers = new ArrayList<>();
         for (int i = 0; i < line.length(); i++) {
@@ -33,9 +44,9 @@ public class HW3_ex1 {
         }
 
         /*
-        Counts all the different numbers in lists. And puts them as key, value pairs into the HashMap.
-        If there is already key for that element, then it adds to the value +1
-        If there is not key for that element, then it puts key and 1 into the HashMap
+        Counts occurrences of each digit character and stores them as key-value pairs in a HashMap.
+        If a character already exists as a key, its corresponding value is incremented by 1. Otherwise,
+        a new key-value pair is added with the character and a count of 1.
          */
         Map<Character, Integer> numberCounts = new HashMap<>();
         for (char n : numbers) {
@@ -47,10 +58,10 @@ public class HW3_ex1 {
         }
 
         /*
-        Goes through the entries in HashMap, and finds key with biggest value. Intially maxCount is 0, and it goes one by one
-        through the HashMap. If next value (currentCount) is bigger, then it replaces char result with specific key and
-        it's value becomes new maxCount. If maxCount == currentCount --> checks if also the key is bigger than the current
-        result.
+        Iterates through the entries in the HashMap to find the digit character with the highest count.
+        Initializes maxCount as 0 and iterates through the HashMap. If the count of the current digit
+        character is greater than maxCount, or if it is equal to maxCount but the character is greater
+        than the current result, updates the result to the current character.
          */
         int maxCount = 0;
         int result = 0;
@@ -64,12 +75,18 @@ public class HW3_ex1 {
         return (char) result;
     }
 
-    /*
-    Would be better name modifyString, as it removes most frequent number AND replaces symbols with "."
-    Using regex to replace symbols
+    /**
+     * Modifies the input string by removing the most frequent digit character and replacing all non-alphanumeric
+     * characters with '.'.
+     *
+     * @param line The input string to be modified.
+     * @param number The most frequent digit character to be removed from the string.
+     * @return The modified string with the most frequent digit character removed and symbols replaced with '.'.
      */
     public static String removeMostFrequentNumber(String line, char number) {
+        // Replaces all non-alphanumeric characters with '.'
         line = line.replaceAll("[^a-zA-Z0-9\\s]", ".");
+        // Removes occurrences of the most frequent digit character from the string
         return line.replace(String.valueOf(number), "");
     }
 }
--- src/HW3_ex2.java
+++ src/HW3_ex2.java
@@ -3,16 +3,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HW3_ex2 {
+    /**
+     * Main method to demonstrate file manipulation operations.
+     * Reads data from the input file, processes it,
+     * and performs line removal based on specific criteria.
+     *
+     * @param args Command-line arguments (not used in this implementation).
+     * @return void
+     */
     public static void main(String[] args) {
         String sisendfail = "src/inimesed.txt";
         File file = new File(sisendfail);
         if (file.exists()) {
             convert(sisendfail);
+            removeLine(sisendfail, "00000000001|Meri|Piret|errr   //vale");
         } else {
             System.out.println("File does not exist: " + sisendfail);
         }
     }
 
+    /**
+     * Reads data from a given input file, processes each line,
+     * and categorizes them into valid and non-valid lines based on a specific structure.
+     * Valid lines contain four elements separated by '|' with the following format:
+     *   - 11 digits (numeric)
+     *   - A word (alphabetic characters)
+     *   - Another word (alphabetic characters)
+     *   - 4 digits (numeric)
+     * Non-valid lines are those that do not adhere to the specified structure.
+     *
+     * @param sisendfail The path to the input file to be processed.
+     * @return void
+     * @throws IOException If an I/O error occurs while reading the file.
+     */
     public static void convert(String sisendfail) {
         List<String> lines = new ArrayList<>();
         try {
@@ -46,12 +69,25 @@ public class HW3_ex2 {
         System.out.println("Valid: " + validLines);
         System.out.println("Non valid: " + nonValidLines);
 
-        writeValidLinesToFile(validLines);
-        writeNonValidLinesToFile(nonValidLines);
+        writeModifiedStructure(validLines, "src/valund1.txt");
+        writeSameStructure(nonValidLines, "src/valjund2.txt");
     }
 
-    public static void writeValidLinesToFile(List<String> validLines) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/valund1.txt"))) {
+    /**
+     * Writes the valid lines to a file with a modified structure, separating each element into labeled fields.
+     * Each valid line is formatted and written to the file with the following structure:
+     *   - Perekonnanimi: [last name]
+     *   - Eesnimi: [first name]
+     *   - Isikukood: [identification number]
+     *   - Palk: [salary]
+     * Each field is separated by a newline character ('\n').
+     *
+     * @param validLines A list containing valid lines to be written to the file with modified structure.
+     * @return void
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public static void writeModifiedStructure(List<String> validLines, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
             for (String line : validLines) {
                 String[] parts = line.split("\\|");
 
@@ -61,22 +97,57 @@ public class HW3_ex2 {
                 writer.write("Palk: " + parts[3] + "\n");
                 writer.write("\n");
             }
+            System.out.println("Valid written in 'src/valund1.txt'");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void writeNonValidLinesToFile(List<String> nonValidLines) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/valjund2.txt"))) {
-            for (String line : nonValidLines) {
-                writer.write(line);
+    /**
+     * Writes the lines to a specified file while maintaining their original structure. Used for non-valid numbers and
+     * @method removeLine
+     *
+     * @param lines A list containing lines to be written to the file.
+     * @param filename The name of the file where the lines will be written.
+     * @return void
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public static void writeSameStructure(List<String> lines, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (String line : lines) {
+                writer.write(line + "\n");
+            }
+            System.out.println("Non valid written in '" + filename + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads lines from a given input file, removes the line that matches the specified string, 
+     * and writes the remaining lines back to the same file.
+     *
+     * @param inputFile The path to the input file from which lines will be removed.
+     * @param target The string representing the line to be removed.
+     * @return void
+     * @throws IOException If an I/O error occurs while reading from or writing to the file.
+     */
+    public static void removeLine(String inputFile, String target) {
+        List<String> lines = new ArrayList<>();
+        try {
+            BufferedReader rd = new BufferedReader(new FileReader(inputFile));
+            String ln = rd.readLine();
+            System.out.println("Removing line: " + target);
+            while (ln != null) {
+                if (!ln.equals(target)) {
+                    lines.add(ln);
+                }
+                ln = rd.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void removeLine(String sisendfail, String otsitav) {
-
+        writeSameStructure(lines, inputFile);
+        System.out.println("Complete finding removable and removed if there was such line!");
     }
 }
